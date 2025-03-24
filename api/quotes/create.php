bcdb617ddb5432989f3a3142c94745e4f72c722d
Caitlin <c_smith24@mail.fhsu.edu>
2025-03-23 19:16:46 -0500

    Fixed errors

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -20,12 +20,12 @@ $data = json_decode(file_get_contents("php://input"));
     
 //Set the properties of quote
 $quote->quote = $data->quote;
-$quote->author_id = $data->author_id;
-$quote->category_id = $data->category_id;
+$author->author_id = $data->author_id;
+$category->category_id = $data->category_id;
 
 //Create quote
 if ($quote->create()) {
-    echo json_encode(array("id" => $quote->id, "quote" => $quote->quote, "author_id" => $author_id->author_id, "category_id" => $category_id->category_id));
+    echo json_encode(array("id" => $quote->id, "quote" => $quote->quote, "author_id" => $author->author_id, "category_id" => $category->category_id));
 } else {
     echo json_encode(
         array('message' => 'Missing Required Parameters'));
